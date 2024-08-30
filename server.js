7d8947e3a9796b2c13cf4a6284c631a88110f4ff
Sandeep Sinha <1233sandeepsinha@gmail.com>
2024-08-30 09:39:56 +0530

    dj

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const app = require('./app');
-
+const cors = require('cors');
+app.use(cors({
+  origin: 'https://job-app-backend-1-8g7u.onrender.com', 
+  optionsSuccessStatus: 200,
+}));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {

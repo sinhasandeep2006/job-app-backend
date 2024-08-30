78e059edc92cf21b564bb1a45f6bdecfd7e8d6ec
Sandeep Sinha <1233sandeepsinha@gmail.com>
2024-08-30 12:10:02 +0530

    jf

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -19,15 +19,19 @@ const upload = multer({ storage: storage });
 // Route to handle user signup
 router.post('/signup', upload.single('resume'), async (req, res) => {
     const { name, email, phone } = req.body;
-    const resume = req.file ? req.file.path : null;
+    const resume = req.file ? req.file.path : null; 
 
     try {
         const newUser = new User({ name, email, phone, resume });
         await newUser.save();
         res.status(201).json({ message: 'User registered successfully!' });
+        alert("user is created")
     } catch (error) {
+        console.error('Error registering user:', error);
         res.status(500).json({ message: 'Error registering user', error });
+        alert("Error registering user")
     }
 });
+
 
 module.exports = router;

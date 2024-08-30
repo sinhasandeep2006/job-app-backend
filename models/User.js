bc1d9909bf6d9aa3631f3db7e9150c4a2fcdfd2e
Sandeep Sinha <1233sandeepsinha@gmail.com>
2024-08-30 12:25:28 +0530

    jf

--- models/User.js
+++ models/User.js
@@ -4,7 +4,8 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
     phone: { type: String, required: true },
-    resume: { type: String, required: true }, // Path to the uploaded resume
+    resume: { type: String, required: false
+    }, // Path to the uploaded resume
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
